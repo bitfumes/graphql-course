5a852c9c942365a31ff606b5a879694629652f9f
sarthaksavvy <bitfumes@gmail.com>
2021-10-13 14:24:09 +0530

    02 GraphQL - use graphiql to run query

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const { buildSchema, graphql } = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
 
 const schema = buildSchema(`
     type Query{
@@ -16,6 +20,19 @@ const root = {
   },
 };
 
-graphql(schema, "{email,name}", root).then((res) => {
-  console.log(res);
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8000);
+
+console.log("Server is running at http://localhost:8000");
+
+// graphql(schema, "{email,name}", root).then((res) => {
+//   console.log(res);
+// });
