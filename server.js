2138ead4cab989c3315ce4b5c94303dc1796e68a
sarthaksavvy <bitfumes@gmail.com>
2021-12-01 11:14:24 +0530

    10 GraphQL - working with fake DB

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const usersData = require("./users.json");
 
 const app = express();
 
-let fakeDb = {};
+let fakeDb = [
+  { id: 1, name: "office", rent: "$25" },
+  { id: 2, name: "co-working", rent: "$10" },
+];
 
 const schema = buildSchema(`
     type Person {
@@ -13,14 +16,21 @@ const schema = buildSchema(`
       email: String
     }
 
+    type Space {
+      name: String
+      rent: String
+    }
+
     type Query{
       users: [Person],
       user(id: Int): Person
       getMsg : String
+      getSpace(id:ID!) : Space !
     }
 
     type Mutation {
       addMsg(msg: String): String
+      createSpace(name:String,rent:String) : Space!
     }
 `);
 
@@ -29,6 +39,12 @@ const root = {
   user: ({ id }) => usersData.find((user) => user.id === id),
   addMsg: ({ msg }) => (fakeDb.message = msg),
   getMsg: () => fakeDb.message,
+  createSpace: ({ name, rent }) =>
+    (fakeDb[fakeDb.length] = { id: fakeDb.length, name, rent }),
+
+  getSpace: ({ id }) => {
+    return fakeDb.find((space) => space.id == id);
+  },
 };
 
 app.use(
